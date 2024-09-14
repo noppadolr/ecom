7736aa945dfa333b4c8c3e9e3bc53df76072398f
noppadolr <riyasarn.n@gmail.com>
2024-09-14 10:23:57 +0700

    database seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Carbon;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +15,37 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name'          =>      'Admin user',
+                'username'      =>      'admin' ,
+                'email'         =>      'admin@gmail.com',
+                'password'      =>      Hash::make('111'),
+                'role'          =>      'admin',
+                'created_at'    =>      Carbon::now(),
+                'email_verified_at'=>Carbon::now(),
+
+            ],
+            [
+                'name'          =>      'Vendor user',
+                'username'      =>      'vendor' ,
+                'email'         =>      'vendor@gmail.com',
+                'password'      =>      Hash::make('111'),
+                'role'          =>      'vendor',
+                'created_at'    =>      Carbon::now(),
+                'email_verified_at'=>Carbon::now(),
+
+            ],
+            [
+                'name'          =>      'User',
+                'username'      =>      'user' ,
+                'email'         =>      'user@gmail.com',
+                'password'      =>      Hash::make('111'),
+                'role'          =>      'user',
+                'created_at'    =>      Carbon::now(),
+                'email_verified_at'=>Carbon::now(),
+
+            ]
+        ]);
     }
 }
